6baf0a351de39c1774692606f6b5aac340d81438
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-04 20:12:34 -0300

    commit inicial

---
+++ Sintaxe basica/Variaveis.java
@@ -0,0 +1,17 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 10;
+        idade = 20;
+        idade = 30;
+
+        String nome = "Thiago Fabri";
+        nome = "Thiago";
+        nome = "Fabri";
+
+        var carro = "Fusca";
+        carro = "Gol";
+
+        var ram = 16;
+        ram = 32;
+    }
+}
