9e880aeb87f1be8d665d0790cf2e5555abbabf89
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-04 21:14:57 -0300

    TiposPrimitivos

--- Sintaxe basica/TiposPrimitivos.java
+++ Sintaxe basica/TiposPrimitivos.java
@@ -1,3 +1,20 @@
 public class TiposPrimitivos {
-    
+    public static void main(String[] args) {
+        // // numeros inteiros = 100, 200, 300, 54854824.
+        // byte => 8 bits => -128 a 127
+        // short => 16 bits => -32,768 a 32,767
+        // int => 32 bits => -2,147,483,648 a 2,147,483,647
+        // long => 64 bits => -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+        // // numeros decimais = 10.5, 20.5, 30.5, 40.5.
+        // float => 32 bits => -3.40282347E+38 a 3.40282347E+38
+        // double => 64 bits => -1.79769313486231570E+308
+
+        // String => representa uma cadeia de caracteres, ou seja, um texto. Ex: "Olá, mundo!"
+        //     char => representa um único caractere, ou seja, uma letra, número ou símbolo. Ex: 'a', '1', '$'
+        //     char meuchar = 'a';
+        //     meuchar = 'Thiago'; // erro, pois char só aceita um caractere
+
+        //     boolean => representa um valor lógico, ou seja, verdadeiro ou falso. Ex: true, false
+
+    }
 }
