0132f1c3ff74c077fcaf33b4776926f40084f252
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-08 20:25:02 -0300

    Vetor

---
+++ Sintaxe basica/Vetor.java
@@ -0,0 +1,12 @@
+public class Vetor {
+    public static void main(String[] args) {
+        int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] vetor2 = new int[10];
+
+        System.out.println(vetor[0]);
+        System.out.println(vetor[9]);
+        System.out.println(vetor.length);
+        System.out.println(vetor2.length);
+
+    }
+}
