85a113012fab98f81b426b02ae22e0663d98b73a
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-08 20:55:57 -0300

    Casting

---
+++ Sintaxe basica/Casting.java
@@ -0,0 +1,15 @@
+public class Casting {
+    public static void main(String[] args) {
+        double d = 9.78;
+        int i = (int) d; // Casting double to int
+
+        int meuInteiro = 10;
+        double meuDouble = (double) meuInteiro; // Casting int to double
+
+        String meuString = "123";
+        int meuInteiro2 = Integer.parseInt(meuString); // Casting String to int
+
+        String meuString2 = String.valueOf(meuInteiro2); // Casting int to String
+    }
+    
+}
